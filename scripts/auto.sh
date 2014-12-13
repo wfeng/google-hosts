25032adf860bb46dae32dcc041f37cb3dc559782
ksafe <ksafer@gmail.com>
2014-12-13 11:59:58 +0800

    auto find 74.125, 173.194

--- scripts/auto.sh
+++ scripts/auto.sh
@@ -7,16 +7,14 @@
 
 if [ $# -eq 0 ]
 then
-    echo -e "Usage:\n"
-    echo -e "    $ ./auto.sh 192.168"
-    echo -e "    $ ./auto.sh 192.168.1"
-    echo -e "    $ ./auto.sh 192.168 192.169 192.170.1"
-    echo -e "\nView auto.sh file to see more.\n"
+    ./find.sh 74.125
+    ./find.sh 173.194
+else
+    for n
+    do
+        ./find.sh $n
+    done
 fi
 
-for n
-do
-    ./find.sh $n
-done
 ./select.sh
 ./apply.sh
